2c0d26f8d7920fc7aa4420167cecd5eb3f87810b
mcardona9015 <mcardona9015@gmail.com>
2021-02-04 23:09:36 -0500

    commit tree 102bf5323d60d133985ff7b0fe38c6fbc9b73fce

--- src/index.js
+++ src/index.js
@@ -1,1 +1,80 @@
-console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+let dogImages
+let dogBreeds
+const dogImageContainer = document.querySelector('div#dog-image-container')
+const dogBreedsContainer = document.querySelector('#dog-breeds')
+
+function fetchImg() {
+    fetch(imgUrl)
+    .then(response => response.json())
+    // .then(data => setDogImages(data))
+    .then(data => {
+        dogImages = data.message
+        dogImages.forEach(image => {
+            let newImg = document.createElement('img')
+            newImg.src = image
+            dogImageContainer.append(newImg)
+        })
+    })
+}
+
+function fetchBreed() {
+    fetch(breedUrl)
+    .then(response => response.json())
+    .then(data => {
+        dogBreeds = Object.keys(data.message)
+        dogBreeds.forEach(breed => {
+            let breedLi = document.createElement('li')
+            breedLi.innerText = breed
+            if (breedLi.innerText[0] != 'a') {breedLi.style.display = 'none'}
+            dogBreedsContainer.append(breedLi)
+        })
+        
+    })
+}
+
+dogBreedsContainer.addEventListener('click', e => {
+    if (e.target.tagName === 'LI') {
+        e.target.style.color = 'firebrick'
+    }
+})
+
+Array.from(dogBreedsContainer.children).forEach(li => { 
+    if(!li.textContent[0] === 'a') {
+        li.style.display = 'none'
+    }
+    else {
+        li.style.display = ''
+    }
+})
+
+const breedDropdown = document.querySelector('#breed-dropdown')
+console.log('breedDropdown: ', breedDropdown);
+breedDropdown.addEventListener('mouseup', e => {
+    Array.from(dogBreedsContainer.children).forEach(li => { 
+        if(li.textContent[0] === e.target.value) {
+            li.style.display = ''
+        }
+        else {
+            li.style.display = 'none'
+
+        }
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchImg()
+    fetchBreed()
+})
+
+// function setDogImages(data){
+//     dogImages = data.message
+// }
+// function setDogBreeds(data){
+//     dogBreeds = data.message
+// }
+
+
+
